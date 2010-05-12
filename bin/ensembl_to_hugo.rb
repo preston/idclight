60f2f031b1077b965262e370665698272350a114
Preston Lee <conmotto@gmail.com>
2010-05-11 17:22:58 -0700

    fixes and ensembl support

---
+++ bin/ensembl_to_hugo.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+current_dir = File.dirname(File.expand_path(__FILE__))
+lib_path = File.join(current_dir, '..', 'lib')
+$LOAD_PATH.unshift lib_path
+
+require 'rubygems'
+
+require 'idclight'
+
+include IDConverter::Light
+
+
+code = 0
+puts "\n"
+puts "IDCLight (IDConverter Light) command-line converter utility, by Preston Lee. (Screen scapes http://idclight.bioinfo.cnio.es for data.)"
+if ARGV.length == 1
+	id = ARGV[0]
+	r = ensembl_id_to_hugo_id(id)
+	if r.nil?
+	  puts "No result. Bad input, perhaps? :("
+	  code = 1
+	else
+		puts "\n#{r}"
+	end
+else
+	puts "\n\tUsage: #{__FILE__} <id>"
+end
+puts "\n"
+
+exit(code)
+
